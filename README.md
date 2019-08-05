88cc6bafaa1579a01da7659ff8322a7ea239e00f
Zsombi55 <47913849+Zsombi55@users.noreply.github.com>
2019-08-05 11:40:12 +0300

    Readme edit & top menu js handlers.

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # Parking-Sharing
-Web application, for sharing parking spots when not used by the owner in owner defined periods.
 
-Public link: https://zsombi55.github.io/Parking-Sharing/
+A simple web application using node js & express for managing parking space booking.
+
+---
+
+Registered users can view both their current bookings as well as a list of available parking spaces, and make reservations.
+
+For the moment only the system administrator (who has access to the database) can register both new users and parking spaces.
+
+## Static live preview
+
+Open [https://zsombi55.github.io/Parking-Sharing/public/index.html](https://zsombi55.github.io/Parking-Sharing/public/index.html)
+
+## Content
+
+Once logged in..
---
+++ public/javascripts/parking.js
@@ -0,0 +1,40 @@
+
+
+// Top-menu handlers:
+function initTopMenu() {	// find all top-menu items and handle their "onclick" events.
+	const links = document.querySelectorAll("#topMenu a");
+	console.info(links);
+
+	for (var i = 0; i < links.length; i++) {
+		if (links[i].hasAttribute("data-page")) {	// don't collect irrelevant anchors.
+			links[i].onclick = clickTmItem;	// no "()" after the function name so the browser calls it not us.
+		} else {
+			continue;
+		}
+	}
+}
+
+function clickTmItem() {	// Click Top Menu Item.
+	console.warn("clicked on menu", this);	// "this" is a variable which was just used. Here: just clicked on.
+	hidePages();
+
+	var pageId = this.getAttribute("data-page");
+	console.warn({ pageId });
+	showPage(pageId);
+}
+
+function hidePages() {
+	var pages = document.querySelectorAll(".page");
+	pages.forEach(function (page) {
+		page.style.display = "none";
+	});
+}
+
+function showPage(page) {	// sets the visibility status of the ID'd object; none = hidden, block = visible.
+	console.warn(page);
+	document.getElementById(page).style.display = "block";
+}
+
+initTopMenu();
+// --END-- Top-menu functions.
+
